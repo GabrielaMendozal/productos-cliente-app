d9aba134d59dbda8e2bcdfe093b2cfc9574b1ab4
GabrielaMendozal <mendozalizethg@gmail.com>
2022-04-17 23:42:31 -0500

    parte ii final

--- src/Componentes/DetalleProducto/DetalleProducto.js
+++ src/Componentes/DetalleProducto/DetalleProducto.js
@@ -3,11 +3,11 @@ import React,{ useEffect, useState } from "react";
 
 const DetalleProducto = (props) => {
     const id = props.match.params.id;
-    const [producto, setProducto] = useState({})
+    const [producto, setProducto] = useState({}) //creo una variable local
     useEffect( () => {  
         axios.get( 'http://localhost:8080/api/producto/getById/' + id)
           .then( response => {
-              setProducto (response.data); console.log(response);
+              setProducto (response.data); console.log(response); //envio solamente el response
             });
         }, []);
     
